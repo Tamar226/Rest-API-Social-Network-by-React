4be54abeeb54155a368ad6abc960243d0c431e29
Tamar226 <tamarlicht3@gmail.com>
2024-01-02 22:58:04 +0200

    start

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+function Welcome() {
+    return (
+        <div>
+            <Routes location={"/Welcome"} >
+            <h1></h1>
+                <Route>
+                    
+            
+                </Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default Welcome
