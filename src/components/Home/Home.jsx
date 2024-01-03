2924f961156c668c4c7b8ac261d9ea4bd498f271
yokheved <yokheved.spr@gmail.com>
2024-01-03 20:43:54 +0200

    fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { NavLink, Route, Router, Routes } from 'react-router-dom'
+import Posts from '../Posts/Posts'
 
 function Home() {
   return (
     <>
-<nav>
-    <NavLink to="/posts">Posts</NavLink>
-    <NavLink to="/todos">Todos</NavLink>
-    <NavLink to="/albums">Albums</NavLink>
+      <nav>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/todos">Todos</NavLink>
+        <NavLink to="/albums">Albums</NavLink>
 
-</nav>
-<Routes>
-  <Route path={"/posts"}/>
-</Routes>
+      </nav>
+      <Routes>
+        <Route path={"/posts"} element={<Posts />} />
+      </Routes>
     </>
   )
 }
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import LogIn from './LogIn'
 function Welcome() {
     return (
         <>
         <h1>.NET</h1>
         <h6><NavLink to={"/login"}>Login</NavLink></h6>
+        <Routes>
+            <Route path='/login' element={<LogIn/>}/>
+        </Routes>
         </>
     )
 }
