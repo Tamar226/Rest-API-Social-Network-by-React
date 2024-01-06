312864c4993e1937853ab6b91a2dc2f427e7639a
yokheved <yokheved.spr@gmail.com>
2024-01-06 19:05:05 +0200

    fixes

--- src/components/General/NavBar.jsx
+++ src/components/General/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
             <h4 ><NavLink to={`/${1}/todos`}>Todos</NavLink></h4>
             <h4 ><NavLink to={`/${1}/albums`}>Albums</NavLink></h4>
             <h4 ><NavLink to={`/${1}/posts`}>Posts</NavLink></h4>
-            <h4 ><NavLink to={`/${1}/home`}>Home</NavLink></h4>
+            <h4 ><NavLink to={`/${1}`}>Home</NavLink></h4>
         </nav>
     )
 }
