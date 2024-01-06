4796a0510b981a881cd74d3008f8b475f9242937
yokheved <yokheved.spr@gmail.com>
2024-01-07 00:03:15 +0200

    home GUI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
-import { NavLink, Route, Router, Routes } from 'react-router-dom'
-import Posts from '../Posts/Posts'
+import { NavLink, useParams } from 'react-router-dom'
 import './homeStyle.css'
 function Home() {
+  const { id } = useParams()
   return (
-    <>
-      <div className='todosHome'>
-        <NavLink to="/todos">Todos</NavLink>
-      </div>
-      <div className='albumsHome'>
-      </div>
-      <div className='postsHome'>
-        </div>
-        <NavLink to="/posts">Posts</NavLink>
-        <Routes>
-          <Route path={"/posts"} element={<Posts />} />
-        </Routes>
-      </>
-      )
+    <div className='home'>
+      <NavLink className={"option todosHome"} to={`/${id}/todos`}>
+        Todos
+      </NavLink>
+      <NavLink className={"option albumsHome"} to={`/${id}/albums`}>
+        Albums
+      </NavLink>
+      <NavLink className={"option postsHome"} to={`/${id}/posts`}>
+        Posts
+      </NavLink>
+    </div>
+  )
 }
 
-      export default Home
+export default Home
