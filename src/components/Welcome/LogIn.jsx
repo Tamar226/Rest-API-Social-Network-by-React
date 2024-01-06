fa173220cfd5eb221dfa3b4bd171479a2ec8503e
Tamar226 <tamarlicht3@gmail.com>
2024-01-07 01:21:50 +0200

    fixes

--- src/components/Welcome/LogIn.jsx
+++ src/components/Welcome/LogIn.jsx
@@ -7,40 +7,39 @@ function LogIn() {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [loginError, setLoginError] = useState('');
-    
-    function handleLogin() {
+
+    async function getUserData() {
         if (!userName || !password) {
             setLoginError('Please fill in all fields.');
             return;
         }
-        async function getUserData() {
-            const response = await fetch(`http://localhost:3000/users/?username=${userName}`);
-            const data = await response.json()
-            if (data[0].website == password) {
-              localStorage.setItem("currentUser", JSON.stringify(data[0]));
-              navigate(`/users/${data[0].id}/home`)
-            }
-            else {
-                setLoginError('Invalid username or password.');
-                  setUserName("") ;
-                  setPassword("");
-            }
-          };
-        
-       
-    }
+        const response = await fetch(`http://localhost:3000/users/?username=${userName}`);
+        const data = await response.json()
+        if (data[0].website == password) {
+            localStorage.setItem("currentUser", JSON.stringify(data[0]));
+            navigate(`/users/${data[0].id}/home`)
+        }
+        else {
+            setLoginError('Invalid username or password.');
+            setUserName("");
+            setPassword("");
+        }
+    };
+
+
+
     return (
         <div>
             <div className='form'>
                 <h2>Login</h2>
-                <label>Email:</label>
-                <input id="addName" type="email" placeholder="Write your email address" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+                <label>User Name:</label>
+                <input id="addName" type="username" placeholder="Write your user name" value={userName} onChange={(e) => setUserName(e.target.value)} /><br />
 
                 <label>Password:</label>
                 <input id="addPassword" type="password" placeholder="Write your password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
-                {/* {loginError && <p style={{ color: 'red' }}>{loginError}</p>} */}
+                {loginError && <p style={{ color: 'red' }}>{loginError}</p>}
                 <div className='connectLogIn'>
-                    <button onClick={() => handleLogin()}>Click to connect</button>
+                    <button onClick={() => getUserData()}>Click to connect</button>
                 </div>
             </div>
         </div>
