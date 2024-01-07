eb66ad2ba4e2375a6ac6b57bcd761c3ccd015d7f
Tamar226 <tamarlicht3@gmail.com>
2024-01-08 00:35:52 +0200

    צצצ

--- src/components/Welcome/Register.jsx
+++ src/components/Welcome/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './formsStyle.css';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+
 
 function Register() {
     const [userName, setUserName] = useState("");
