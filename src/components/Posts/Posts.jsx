e54ce453a4728f54b79593804b7256b3dd6724c3
Tamar226 <tamarlicht3@gmail.com>
2024-01-12 12:44:00 +0200

    more...

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import './postsStyle.css'
 import PostTitle from './PostTitle'
+import AddPost from './AddPost'
+// import PostMain from './PostMain'
 import { useParams } from 'react-router-dom'
+import PostMain from './PostMain'
+
 export default function Posts() {
   const { id } = useParams();
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);  
+  const [search, setSearch] = useState('');
+  const [showAdd, setShowAdd] = useState(false);
+  // const [currentPage, setCurrentPage] = useState(1);
+
+  function handleSearch() {
+    fetch(`http://localhost:3000/posts/?userId=${id}`)
+      .then(response => response.json())
+      .then(json => {
+        setPosts(json);
+      }).catch(error => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     async function loadPosts() {
       fetch(`http://localhost:3000/posts/?userId=${id}`)
@@ -16,9 +34,15 @@ export default function Posts() {
   }, []);
   return (
     <div className='posts'>
+      <button className='addTodoButton' onClick={() => setShowAdd(true)}>+</button>
+      {showAdd && <AddPost setShowAdd={setShowAdd} setPosts={setPosts} />}
+       <input type="search" className='searchPost' placeholder='search...'
+        onChange={e => setSearch(e.target.value)} value={search} onSubmit={handleSearch} />
+      <button type="submit" className='searchButton' onClick={handleSearch}>🔍</button>
       {posts.map(p =>
         <PostTitle postId={p.id} title={p.title} key={p.id}/>
       )}
+      {/* <button onClick={() => setCurrentPage(p => p + 1)}>⬅️</button><button onClick={() => setCurrentPage(p => p == 1 ? 1 : p - 1)}>➡️</button> */}
     </div>
   )
 }
