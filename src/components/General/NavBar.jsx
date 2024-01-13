dfb7f0f2a047300e927319485f584cd9bd8050cd
yokheved <yokheved.spr@gmail.com>
2024-01-13 21:00:31 +0200

    css

--- src/components/General/NavBar.jsx
+++ src/components/General/NavBar.jsx
@@ -2,28 +2,26 @@ import React, {useEffect, useState} from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
 function NavBar() {
-    const [userIn, setUserIn] = useState(false);
+    const [userIn, setUserIn] = useState(0);
     const location = useLocation();
     let id;
     useEffect(() => { 
         let json = localStorage.getItem('currentUser');
         if(json){
-            id = JSON.parse(json)?.id;
-            console.log(id);
-            setUserIn(true);
+            setUserIn(JSON.parse(json)?.id);
         }
         else{
-            setUserIn(false);
+            setUserIn(0);
         }
     }, [location.pathname]);
     return (
         <nav>
             <h1>.NET</h1>
-            {userIn && <><h4 ><NavLink to={`/${1}/todos`}>Todos</NavLink></h4>
-            <h4 ><NavLink to={`/${1}/albums`}>Albums</NavLink></h4>
-            <h4 ><NavLink to={`/${1}/posts`}>Posts</NavLink></h4>
-            <h4 ><NavLink to={`/${1}`}>Home</NavLink></h4>
-            <h4 ><NavLink to={`/${1}/info`}>Info</NavLink></h4></>}
+            {userIn && <><h4 ><NavLink to={`/${userIn}/todos`}>Todos</NavLink></h4>
+            <h4 ><NavLink to={`/${userIn}/albums`}>Albums</NavLink></h4>
+            <h4 ><NavLink to={`/${userIn}/posts`}>Posts</NavLink></h4>
+            <h4 ><NavLink to={`/${userIn}`}>Home</NavLink></h4>
+            <h4 ><NavLink to={`/${userIn}/info`}>Info</NavLink></h4></>}
         </nav>
     )
 }
