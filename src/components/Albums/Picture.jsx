6f5d49a04f73e39c272a782c49789cb761497608
yokheved <yokheved.spr@gmail.com>
2024-01-14 22:43:39 +0200

    picture update

--- src/components/Albums/Picture.jsx
+++ src/components/Albums/Picture.jsx
@@ -1,26 +1,35 @@
-import React ,{useState}from 'react'
+import React ,{useEffect, useState}from 'react'
 import { useParams } from 'react-router-dom'
 import PictureEdit from './PictureEdit';
 import './AlbumsStyle.css'
 function Picture({ picture,setPictures }) {
   const [showUpdate, setShowUpdate] = useState(false);
+  const [thisPicture, setThisPicture] = useState(picture);
+
+    useEffect(() => {
+    fetch(`http://localhost:3000/photos/${picture.id}`)
+      .then(response => response.json())
+      .then(json => {
+        setThisPicture(json);
+      })
+      .catch(e => console.log(e))
+    }, [showUpdate]);
 
   function handleDelete() {
-    fetch(`http://localhost:3000/photos/${picture.id}`, {
+    fetch(`http://localhost:3000/photos/${thisPicture.id}`, {
       method: 'DELETE',
     })
       .then(response => response.json())
       .then(json => {
         console.log(json);
       })
-      setPictures(pictures => pictures.filter(picture1 => picture1.id !== picture.id));
-
+      setPictures(pictures => pictures.filter(picture1 => picture1.id !== thisPicture.id));
   }
   return (
     <div className='photo'>
-      <img src={picture.thumbnailUrl} alt={picture.title} />
-      <h4>{picture.title}</h4>
-      {showUpdate && <PictureEdit picture={picture} setShowEdit={setShowUpdate} />}
+      <img src={thisPicture.thumbnailUrl} alt={thisPicture.title} />
+      <h4>{thisPicture.title}</h4>
+      {showUpdate && <PictureEdit picture={thisPicture} setShowEdit={setShowUpdate} />}
       <button onClick={() => setShowUpdate(true)}>✒️</button>
       <button onClick={handleDelete}>🗑️</button>
     </div>
