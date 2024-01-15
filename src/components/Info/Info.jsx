b25a95ee5381d2fc39bbfc3476e241c0d6c24f65
Tamar226 <tamarlicht3@gmail.com>
2024-01-15 12:48:54 +0200

    style information

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import './infoStyle.css'
 function Info() {
     const currentuser = JSON.parse(localStorage.getItem('currentUser'));
     return (
         <div>
             <h1>Hello, {currentuser.name}.</h1>
             <h3>What do we know about you? 🕵️</h3>
-            <div>
-                <p>id: {currentuser.id}</p>
-                <p>name: {currentuser.name}</p>
-                <p>username: {currentuser.username}</p>
-                <p>email: {currentuser.email}</p>
-                <p>address:</p>
+            <div style={{fontWeight:'bold'}} className='information'>
+                <p>id: {currentuser.id}<br/>
+                name: {currentuser.name}<br/>
+                username: {currentuser.username}<br/>
+                email: {currentuser.email}<br/>
+                address:</p>
                 <ul>
                     <li>street: {currentuser.address.street}</li>
                     <li>suite: {currentuser.address.suite}</li>
